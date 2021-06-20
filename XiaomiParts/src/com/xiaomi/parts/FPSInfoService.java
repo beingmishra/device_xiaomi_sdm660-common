PalladiumOS-Devices/device_xiaomi_whyred-sdm660-common

sdm660-common: XiaomiParts : FPS Info: Redesign FPS Info

XiaomiParts/src/com/xiaomi/parts/FPSInfoService.java
@@ -90,20 +90,19 @@ public class FPSInfoService extends Service {
         FPSView(Context c) {
             super(c);
             float density = c.getResources().getDisplayMetrics().density;
-            int paddingPx = Math.round(5 * density);
+            int paddingPx = Math.round(10 * density);
             setPadding(paddingPx, paddingPx, paddingPx, paddingPx);
-            setBackgroundColor(Color.argb(0x60, 0, 0, 0));
+            setBackgroundColor(Color.argb(0x0, 0, 0, 0));
 
-            final int textSize = Math.round(18 * density);
-
-            Typeface typeface = Typeface.create("sans-serif-condensed", Typeface.BOLD);
+            final int textSize = Math.round(20 * density);
+            Typeface typeface = Typeface.create("google-sans", Typeface.BOLD);
 
             mOnlinePaint = new Paint();
             mOnlinePaint.setTypeface(typeface);
             mOnlinePaint.setAntiAlias(true);
             mOnlinePaint.setTextSize(textSize);
-            mOnlinePaint.setColor(Color.WHITE);
-            mOnlinePaint.setShadowLayer(8.0f, 0.0f, 0.0f, Color.BLACK);
+            mOnlinePaint.setColor(Color.YELLOW);
+            mOnlinePaint.setShadowLayer(5.0f, 0.0f, 0.0f, Color.BLACK);
 
             mAscent = mOnlinePaint.ascent();
             float descent = mOnlinePaint.descent();
@@ -163,7 +162,7 @@ public class FPSInfoService extends Service {
                 return;
             }
 
-            int neededWidth = mPaddingLeft + mPaddingRight + mMaxWidth;
+            int neededWidth = mPaddingLeft + mPaddingRight + mMaxWidth + 40;
             int neededHeight = mPaddingTop + mPaddingBottom + 40;
             if (neededWidth != mNeededWidth || neededHeight != mNeededHeight) {
                 mNeededWidth = neededWidth;

beingmishra/device_xiaomi_sdm660-common

XiaomiParts/src/com/xiaomi/parts/FPSInfoService.java
@@ -90,20 +90,19 @@ public class FPSInfoService extends Service {
         FPSView(Context c) {
             super(c);
             float density = c.getResources().getDisplayMetrics().density;
-            int paddingPx = Math.round(5 * density);
+            int paddingPx = Math.round(10 * density);
             setPadding(paddingPx, paddingPx, paddingPx, paddingPx);
-            setBackgroundColor(Color.argb(0x60, 0, 0, 0));
+            setBackgroundColor(Color.argb(0x0, 0, 0, 0));
 
-            final int textSize = Math.round(18 * density);
-
-            Typeface typeface = Typeface.create("sans-serif-condensed", Typeface.BOLD);
+            final int textSize = Math.round(20 * density);
+            Typeface typeface = Typeface.create("google-sans", Typeface.BOLD);
 
             mOnlinePaint = new Paint();
             mOnlinePaint.setTypeface(typeface);
             mOnlinePaint.setAntiAlias(true);
             mOnlinePaint.setTextSize(textSize);
-            mOnlinePaint.setColor(Color.WHITE);
-            mOnlinePaint.setShadowLayer(8.0f, 0.0f, 0.0f, Color.BLACK);
+            mOnlinePaint.setColor(Color.RED);
+            mOnlinePaint.setShadowLayer(5.0f, 0.0f, 0.0f, Color.BLACK);
 
             mAscent = mOnlinePaint.ascent();
             float descent = mOnlinePaint.descent();
@@ -163,7 +162,7 @@ public class FPSInfoService extends Service {
                 return;
             }
 
-            int neededWidth = mPaddingLeft + mPaddingRight + mMaxWidth;
+            int neededWidth = mPaddingLeft + mPaddingRight + mMaxWidth + 40;
             int neededHeight = mPaddingTop + mPaddingBottom + 40;
             if (neededWidth != mNeededWidth || neededHeight != mNeededHeight) {
                 mNeededWidth = neededWidth;